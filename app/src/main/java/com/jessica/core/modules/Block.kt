91a4653fcfccd95191551d41596335def836fe7e
Kamara229 <abduld10kareem@gmail.com>
2026-09-05 00:47:24 +0300

    Update Block.kt

--- app/src/main/java/com/jessica/core/modules/Block.kt
+++ app/src/main/java/com/jessica/core/modules/Block.kt
@@ -1,7 +1,21 @@
 package com.jessica.core.modules
 
 data class Block(
+
+    val id: String,
+
     val name: String,
+
     val version: String,
-    val status: String
+
+    val author: String,
+
+    val type: String,
+
+    val status: String,
+
+    val description: String,
+
+    val capabilities: List<String>
+
 )
