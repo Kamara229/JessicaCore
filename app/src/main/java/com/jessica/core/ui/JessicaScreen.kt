efb55af1a04942bc8e2c1404b8ea7801e1732ab9
Kamara229 <abduld10kareem@gmail.com>
2026-09-08 00:09:17 +0300

    Update JessicaScreen.kt

--- app/src/main/java/com/jessica/core/ui/JessicaScreen.kt
+++ app/src/main/java/com/jessica/core/ui/JessicaScreen.kt
@@ -1,21 +1,42 @@
 package com.jessica.core.ui
 
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+
+import androidx.lifecycle.viewmodel.compose.viewModel
+
 import androidx.compose.runtime.collectAsState
 
 import com.jessica.core.core.JessicaController
 import com.jessica.core.navigation.JessicaNavigator
 import com.jessica.core.navigation.JessicaPage
+
+import com.jessica.core.modules.blocks.BlockFileLoader
+
 import com.jessica.core.modules.updater.UpdateViewModel
 
-import androidx.lifecycle.viewmodel.compose.viewModel
+import com.jessica.core.ui.home.HomeScreen
+
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 
 
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun JessicaScreen() {
 
@@ -56,65 +77,282 @@ fun JessicaScreen() {
 
 
 
-    when (
-        navigator.currentPage.value
-    ) {
+    LaunchedEffect(Unit) {
+
+        controller.loadBlocks()
+
+    }
 
 
-        JessicaPage.HOME -> {
+
+    val blockPicker =
+        rememberLauncherForActivityResult(
+
+            contract =
+                ActivityResultContracts.OpenDocument()
+
+        ) { uri ->
 
 
-            HomeScreenPlaceholder()
+            if (uri != null) {
+
+
+                val block =
+                    BlockFileLoader.read(
+                        context,
+                        uri
+                    )
+
+
+                if (block != null) {
+
+
+                    controller.installBlock(
+                        block
+                    )
+
+
+                }
+
+
+            }
 
 
         }
 
 
-        JessicaPage.BLOCKS -> {
+
+    Scaffold(
+
+        topBar = {
 
 
-            HomeScreenPlaceholder()
+            TopAppBar(
+
+                title = {
+
+
+                    Text(
+                        "Jessica Core"
+                    )
+
+
+                }
+
+
+            )
 
 
         }
 
 
-        JessicaPage.REPORTS -> {
+    ) { padding ->
 
 
-            HomeScreenPlaceholder()
+
+        Box(
+
+            modifier =
+                Modifier
+                    .padding(padding)
+                    .fillMaxSize()
+
+        ) {
 
 
-        }
+
+            when (
+
+                navigator.currentPage.value
+
+            ) {
 
 
-        JessicaPage.MEMORY -> {
+
+                JessicaPage.HOME -> {
 
 
-            HomeScreenPlaceholder()
+                    HomeScreen(
+
+                        message =
+                            controller.message.value,
 
 
-        }
+                        blockCount =
+                            controller.blocks.value.size,
 
 
-        JessicaPage.TASKS -> {
+                        updateState =
+                            updateState,
 
 
-            HomeScreenPlaceholder()
+                        onUpdate = {
+
+                            updateViewModel
+                                .checkUpdate()
+
+                        },
+
+
+                        onBlocks = {
+
+                            navigator.navigateTo(
+                                JessicaPage.BLOCKS
+                            )
+
+                        },
+
+
+                        onReports = {
+
+                            navigator.navigateTo(
+                                JessicaPage.REPORTS
+                            )
+
+                        },
+
+
+                        onMemory = {
+
+                            navigator.navigateTo(
+                                JessicaPage.MEMORY
+                            )
+
+                        },
+
+
+                        onTasks = {
+
+                            navigator.navigateTo(
+                                JessicaPage.TASKS
+                            )
+
+                        },
+
+
+                        onAddBlock = {
+
+
+                            blockPicker.launch(
+
+                                arrayOf(
+                                    "application/json",
+                                    "*/*"
+                                )
+
+                            )
+
+
+                        }
+
+
+                    )
+
+
+                }
+
+
+
+                JessicaPage.BLOCKS -> {
+
+
+                    BlockScreen(
+
+                        blockManager =
+                            controller.blockManager,
+
+
+                        reportStorage =
+                            controller.reportStorage,
+
+
+                        onUpdate = {
+
+                            controller
+                                .loadBlocks()
+
+                        },
+
+
+                        onBack = {
+
+                            navigator.backHome()
+
+                        }
+
+
+                    )
+
+
+                }
+
+
+
+                JessicaPage.REPORTS -> {
+
+
+                    ReportScreen(
+
+                        reportStorage =
+                            controller.reportStorage,
+
+
+                        onBack = {
+
+                            navigator.backHome()
+
+                        }
+
+
+                    )
+
+
+                }
+
+
+
+                JessicaPage.MEMORY -> {
+
+
+                    MemoryScreen(
+
+                        onBack = {
+
+                            navigator.backHome()
+
+                        }
+
+                    )
+
+
+                }
+
+
+
+                JessicaPage.TASKS -> {
+
+
+                    TaskScreen(
+
+                        onBack = {
+
+                            navigator.backHome()
+
+                        }
+
+                    )
+
+
+                }
+
+
+            }
 
 
         }
 
 
     }
-
-
-}
-
-
-
-@Composable
-private fun HomeScreenPlaceholder() {
 
 
 }
