4bcbcbdd26b9be3c7b33bd56dbdf03370240066f
Kamara229 <abduld10kareem@gmail.com>
2026-09-07 23:19:01 +0300

    Create UpdateState.kt

---
+++ app/src/main/java/com/jessica/core/modules/updater/UpdateState.kt
@@ -0,0 +1,27 @@
+package com.jessica.core.modules.updater
+
+
+sealed class UpdateState {
+
+
+    object Idle : UpdateState()
+
+
+    object Checking : UpdateState()
+
+
+    data class Downloading(
+        val progress: Int
+    ) : UpdateState()
+
+
+    data class Completed(
+        val version: String
+    ) : UpdateState()
+
+
+    data class Error(
+        val message: String
+    ) : UpdateState()
+
+}
